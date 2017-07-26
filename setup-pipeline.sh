6ad275acc6c51fb34e87e8cec3c3be22d4ba4b01
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-07-26 14:19:41 -0300

    curl --fail

--- setup-pipeline.sh
+++ setup-pipeline.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 org="$1"
 repo="$2"
 echo "~~~ Creating pipeline"
-curl -s -H "Authorization: Bearer $BUILDKITE_TOKEN" -XPOST \
+curl --fail -s -H "Authorization: Bearer $BUILDKITE_TOKEN" -XPOST \
   "https://api.buildkite.com/v2/organizations/$org/pipelines" \
   -d @<(sed -e "s/\$org/$org/g" -e "s/\$repo/$repo/g" pipeline.json)
 
@@ -13,6 +13,6 @@ url="$(\
   "https://api.buildkite.com/v2/organizations/$org/pipelines/$repo" |
   jq -r '.provider.webhook_url' \
 )"
-curl -s -H "Authorization: token $GITHUB_TOKEN" -XPOST \
+curl --fail -s -H "Authorization: token $GITHUB_TOKEN" -XPOST \
   "https://api.github.com/repos/$org/$repo/hooks" \
   -d @<(sed "s#\$url#$url#" webhook.json)
