bcb5c77692e4da816e4a65e4eba8cfe523bb3de0
Carlos Alexandro Becker <caarlos0@gmail.com>
2017-09-28 17:35:17 -0300

    maybe this fixes things, maybe not. idk

--- setup-pipeline.sh
+++ setup-pipeline.sh
@@ -4,15 +4,20 @@ org="$1"
 repo="$2"
 echo "~~~ Creating pipeline"
 curl --fail -s -H "Authorization: Bearer $BUILDKITE_TOKEN" -XPOST \
-  "https://api.buildkite.com/v2/organizations/$org/pipelines" \
-  -d @<(sed -e "s/\$org/$org/g" -e "s/\$repo/$repo/g" pipeline.json)
+	"https://api.buildkite.com/v2/organizations/$org/pipelines" \
+	-d @<(sed -e "s/\$org/$org/g" -e "s/\$repo/$repo/g" pipeline.json)
+
+# YEAS, THIS IS CRAP BUT YEAH WATF
+curl --fail -s -H "Authorization: Bearer $BUILDKITE_TOKEN" -XPATCH \
+	"https://api.buildkite.com/v2/organizations/{org.slug}/pipelines/{slug}" \
+	-d @<(sed -e "s/\$org/$org/g" -e "s/\$repo/$repo/g" pipeline.json)
 
 echo "~~~ Setting up webhooks"
-url="$(\
-  curl -s -H "Authorization: Bearer $BUILDKITE_TOKEN" \
-  "https://api.buildkite.com/v2/organizations/$org/pipelines/$repo" |
-  jq -r '.provider.webhook_url' \
+url="$(
+	curl -s -H "Authorization: Bearer $BUILDKITE_TOKEN" \
+		"https://api.buildkite.com/v2/organizations/$org/pipelines/$repo" |
+		jq -r '.provider.webhook_url'
 )"
 curl --fail -s -H "Authorization: token $GITHUB_TOKEN" -XPOST \
-  "https://api.github.com/repos/$org/$repo/hooks" \
-  -d @<(sed "s#\$url#$url#" webhook.json)
+	"https://api.github.com/repos/$org/$repo/hooks" \
+	-d @<(sed "s#\$url#$url#" webhook.json)
